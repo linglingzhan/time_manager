5de6a947390d6e6fe3503d5395e920d751b549ea
linglingzhan <972696347@qq.com>
2020-11-16 22:30:50 +0800

    listen any addr

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5@t#cc($fsx)#&ph%t8qwz@yvyhd16ut_!z4c^@ca+f)$-yoms'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
